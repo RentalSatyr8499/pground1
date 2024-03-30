9a7cae665dc94094aeb82cef50cdc4fa00ea8dfb
RentalSatyr8499 <87041733+RentalSatyr8499@users.noreply.github.com>
2024-03-30 16:23:59 -0400

    more firebase work
    I GOT FIREBASE TO WORK!! now i need to use it to hide a javascript script. im testing it out using rainbow circle.

---
+++ rainbow circle.js
@@ -0,0 +1,30 @@
+const canvas = document.getElementById("rainbowCircle");
+const ctx = canvas.getContext("2d");
+const radius = 100;
+const centerX = canvas.width / 2;
+const centerY = canvas.height / 2;
+const colors = ["red", "orange", "yellow", "green", "blue", "purple"];
+let currentColorIndex = 0;
+
+function drawCircle() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+    ctx.fillStyle = colors[currentColorIndex];
+    ctx.fill();
+    ctx.closePath();
+}
+
+function changeColor() {
+    currentColorIndex = (currentColorIndex + 1) % colors.length;
+    drawCircle();
+}
+
+setInterval(changeColor, 500); // Change color every 0.5 seconds
+
+// Set canvas size
+canvas.width = 2 * radius;
+canvas.height = 2 * radius;
+
+// Initial draw
+drawCircle();
