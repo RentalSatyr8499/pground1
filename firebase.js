5b3f2e2ad6478ac2d471fe5c5d8eb2ca250f7e8b
RentalSatyr8499 <87041733+RentalSatyr8499@users.noreply.github.com>
2024-03-30 15:57:11 -0400

    finished adding more firebase stuff
    still following the same tutorial (turns out ai did a lot of things right)

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyASA1As8H5WwkUnuEMssAIsaJjupSxSH3Q",
+    authDomain: "playground-6b9e0.firebaseapp.com",
+    databaseURL: "https://playground-6b9e0-default-rtdb.firebaseio.com",
+    projectId: "playground-6b9e0",
+    storageBucket: "playground-6b9e0.appspot.com",
+    messagingSenderId: "658513536207",
+    appId: "1:658513536207:web:5b92ec834aed2bee9b43ba",
+    measurementId: "G-Q4755700KL"
+  };
+
+
