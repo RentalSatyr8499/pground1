001a826070e59e0d0579bba487b4996b3a46e29e
RentalSatyr8499 <87041733+RentalSatyr8499@users.noreply.github.com>
2024-04-04 11:15:54 -0400

    last attempt to use dom to switch scenes
    start menu is not up to date in this push

---
+++ level 1.js
@@ -0,0 +1,4 @@
+alert("hello!!!");
+ctx.fillStyle = "black";
+ctx.font = "30px Arial";
+ctx.fillText("PENIS!", 250, 250);
--- start menu.js
+++ start menu.js
@@ -10,13 +10,13 @@ var boxSpacing = ((Math.min(canvas.width, canvas.height))*(marginPercent)) / 4;
 
 // Create a 3x3 array with dictionaries
 var levelDisplay = [
-    [{"text": "Day 1", "color": "mediumpurple", "on": true}, {"text": "Day 2", "color": "limegreen", "on": false}, {"text": "Day 3", "color": "deepskyblue", "on": false}],
-    [{"text": "Day 4", "color": "gold", "on": true}, {"text": "Day 5", "color": "mediumpurple", "on": false}, {"text": "Day 6", "color": "darkorange", "on": false}],
-    [{"text": "Day 7", "color": "lightpink", "on": true}, {"text": "Day 8", "color": "mediumturquoise", "on": false}, {"text": "Day 9", "color": "dimgray", "on": false}]
+    [{"text": "Day 1", "color": "mediumpurple", "on": true, "script": "level 1.js"}, {"text": "Day 2", "color": "limegreen", "on": false, "script": "level 1.js"}, {"text": "Day 3", "color": "deepskyblue", "on": false, "script": "level 1.js"}],
+    [{"text": "Day 4", "color": "gold", "on": true, "script": "level 1.js"}, {"text": "Day 5", "color": "mediumpurple", "on": false, "script": "level 1.js"}, {"text": "Day 6", "color": "darkorange", "on": false, "script": "level 1.js"}],
+    [{"text": "Day 7", "color": "lightpink", "on": true, "script": "level 1.js"}, {"text": "Day 8", "color": "mediumturquoise", "on": false, "script": "level 1.js"}, {"text": "Day 9", "color": "dimgray", "on": false, "script": "level 1.js"}]
 ];
 
 // Draw boxes and display text
-function drawBoxes() {
+function drawStartMenu() {
 
     // turn boxes off according to day variable
     boxesOn = 0;
@@ -57,11 +57,15 @@ function drawBoxes() {
         }
     }
 }
-drawBoxes();
+drawStartMenu();
 
+function addStartMenuEventListeners(){
+    // Add hover and click functionality
+    canvas.addEventListener("mousemove", handleMouseInteraction);
+    canvas.addEventListener("click", handleMouseInteraction);
+}
 
-// Add hover functionality
-canvas.addEventListener("mousemove", function(event) {
+function handleMouseInteraction(event){
     var rect = canvas.getBoundingClientRect();
     var mouseX = event.clientX - rect.left;
     var mouseY = event.clientY - rect.top;
@@ -71,11 +75,9 @@ canvas.addEventListener("mousemove", function(event) {
             var x = i * (boxSize + boxSpacing) - (boxSize / 2); 
             var y = j * (boxSize + boxSpacing) - (boxSize / 2);
             var levelInfo = levelDisplay[j - 1][i - 1];
+            var mouseInBox = (mouseX > x - (boxSize / 2) && mouseX < x + (boxSize / 2) && mouseY > y - (boxSize / 2) && mouseY < y + (boxSize / 2))
 
-            if (levelInfo.on) {
-                // Check if mouse is inside the current box
-                if (mouseX > x - (boxSize / 2) && mouseX < x + (boxSize / 2) &&
-                    mouseY > y - (boxSize / 2) && mouseY < y + (boxSize / 2)) {
+            if (levelInfo.on && mouseInBox) {
                     // Change box color on hover (you can replace "yellow" with your desired hover color)
                     ctx.fillStyle = "yellow";
                     ctx.fillRect(x - (boxSize / 2), y - (boxSize / 2), boxSize, boxSize);
@@ -85,7 +87,11 @@ canvas.addEventListener("mousemove", function(event) {
                     ctx.textAlign = "center";
                     ctx.textBaseline = "middle";
                     ctx.fillText(levelInfo.text, x, y);
-                } else {
+
+                    if(event.type == "click"){
+                        switchScenes("startMenu", "level1", "level1.js");
+                    }
+                } else if (levelInfo.on) {
                     // Reset box color
                     ctx.fillStyle = levelInfo.color;
                     ctx.fillRect(x - (boxSize / 2), y - (boxSize / 2), boxSize, boxSize);
@@ -98,27 +104,5 @@ canvas.addEventListener("mousemove", function(event) {
                 }
             }
         }
-    }
-});
-
-// Add click functionality
-canvas.addEventListener("click", function(event) {
-    var rect = canvas.getBoundingClientRect();
-    var mouseX = event.clientX - rect.left;
-    var mouseY = event.clientY - rect.top;
-
-    for (var j = 1; j <= 3; j++) {
-        for (var i = 1; i <= 3; i++) {
-            var x = i * (boxSize + boxSpacing) - (boxSize / 2); 
-            var y = j * (boxSize + boxSpacing) - (boxSize / 2);
-            var levelInfo = levelDisplay[j - 1][i - 1];
-
-            if ((levelInfo.on) && (mouseX > x - (boxSize / 2) && mouseX < x + (boxSize / 2) && mouseY > y - (boxSize / 2) && mouseY < y + (boxSize / 2))) {
-                alert(levelInfo["text"])
-            }
-            }
-        }
-    }
-);
-
+};
 
