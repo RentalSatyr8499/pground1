9fd83ae30c9b861c64c8d1df744dbf071004b024
RentalSatyr8499 <87041733+RentalSatyr8499@users.noreply.github.com>
2024-04-11 11:06:59 -0400

    piloting for L6
    stopping and starting circle

--- start menu.js
+++ start menu.js
@@ -1,109 +1,71 @@
-var marginPercent = .2; // percent of canvas that will be taken up by the spacing between boxes
-var day = 7; 
+let canvas = document.getElementById('myCanvas');
+let ctx = canvas.getContext('2d');
 
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
+let redCircle = { x: 250, y: 250, radius: 20, color: 'red' };
 
-// Calculate box dimensions
-var boxSize = ((Math.min(canvas.width, canvas.height))*(1 - marginPercent)) / 3;
-var boxSpacing = ((Math.min(canvas.width, canvas.height))*(marginPercent)) / 4;
-
-// Create a 3x3 array with dictionaries
-var levelDisplay = [
-    [{"text": "Day 1", "color": "mediumpurple", "on": true, "script": "level 1.js"}, {"text": "Day 2", "color": "limegreen", "on": false, "script": "level 1.js"}, {"text": "Day 3", "color": "deepskyblue", "on": false, "script": "level 1.js"}],
-    [{"text": "Day 4", "color": "gold", "on": true, "script": "level 1.js"}, {"text": "Day 5", "color": "mediumpurple", "on": false, "script": "level 1.js"}, {"text": "Day 6", "color": "darkorange", "on": false, "script": "level 1.js"}],
-    [{"text": "Day 7", "color": "lightpink", "on": true, "script": "level 1.js"}, {"text": "Day 8", "color": "mediumturquoise", "on": false, "script": "level 1.js"}, {"text": "Day 9", "color": "dimgray", "on": false, "script": "level 1.js"}]
+let circles = [
+    { x: Math.random() * canvas.width, y: 250, radius: 20, color: 'blue', dx: 2 },
+    { x: Math.random() * canvas.width, y: 250, radius: 20, color: getRandomColor(), dx: 2 },
+    { x: Math.random() * canvas.width, y: 250, radius: 20, color: getRandomColor(), dx: 2 }
 ];
 
-// Draw boxes and display text
-function drawStartMenu() {
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
-    // turn boxes off according to day variable
-    boxesOn = 0;
-    for (var j = 1; j <= 3; j++) {
-        for (var i = 1; i <= 3; i++) {
-                if (boxesOn < day) {
-                    levelDisplay[j-1][i-1]["on"] = true;
-                    boxesOn += 1;
-                }
-                else {
-                    levelDisplay[j-1][i-1]["on"] = false;
-                }
-        }};
+function drawCircle(circle) {
+    ctx.beginPath();
+    ctx.arc(circle.x, circle.y, circle.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = circle.color;
+    ctx.fill();
+}
 
-    for (var j = 1; j <= 3; j++) {
-        for (var i = 1; i <= 3; i++) {
-            var x = i * (boxSize + boxSpacing) - (boxSize / 2); 
-            var y = j * (boxSize + boxSpacing) - (boxSize / 2);
+function update() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-            // Get dictionary from array
-            var levelInfo = levelDisplay[j - 1][i - 1];
-            
-            if (levelInfo.on) {
-                // Draw box
-                ctx.fillStyle = levelInfo.color;
-                ctx.fillRect(x - (boxSize / 2), y - (boxSize / 2), boxSize, boxSize);
-
-                // Display text
-                ctx.fillStyle = "black";
-                ctx.font = "30px Arial";
-                ctx.textAlign = "center";
-                ctx.textBaseline = "middle";
-                ctx.fillText(levelInfo.text, x, y);
-            } else {
-                ctx.fillStyle = "black";
-                ctx.fillRect(x - (boxSize / 2), y - (boxSize / 2), boxSize, boxSize);
+    drawCircle(redCircle);
+    for (let i = 0; i < circles.length; i++) {
+        if (circles[i].dx !== 0) {
+            drawCircle(circles[i]);
+            circles[i].x += circles[i].dx;
+            if (circles[i].x + circles[i].radius > canvas.width || circles[i].x - circles[i].radius < 0) {
+                circles[i].dx = -circles[i].dx;
             }
         }
     }
 }
-drawStartMenu();
 
-function addStartMenuEventListeners(){
-    // Add hover and click functionality
-    canvas.addEventListener("mousemove", handleMouseInteraction);
-    canvas.addEventListener("click", handleMouseInteraction);
-}
-addStartMenuEventListeners();
+setInterval(update, 10);
 
-function handleMouseInteraction(event){
-    var rect = canvas.getBoundingClientRect();
-    var mouseX = event.clientX - rect.left;
-    var mouseY = event.clientY - rect.top;
-
-    for (var j = 1; j <= 3; j++) {
-        for (var i = 1; i <= 3; i++) {
-            var x = i * (boxSize + boxSpacing) - (boxSize / 2); 
-            var y = j * (boxSize + boxSpacing) - (boxSize / 2);
-            var levelInfo = levelDisplay[j - 1][i - 1];
-            var mouseInBox = (mouseX > x - (boxSize / 2) && mouseX < x + (boxSize / 2) && mouseY > y - (boxSize / 2) && mouseY < y + (boxSize / 2))
-
-            if (levelInfo.on && mouseInBox) {
-                    // Change box color on hover (you can replace "yellow" with your desired hover color)
-                    ctx.fillStyle = "yellow";
-                    ctx.fillRect(x - (boxSize / 2), y - (boxSize / 2), boxSize, boxSize);
-                    
-                    ctx.fillStyle = "black";
-                    ctx.font = "30px Arial";
-                    ctx.textAlign = "center";
-                    ctx.textBaseline = "middle";
-                    ctx.fillText(levelInfo.text, x, y);
-
-                    if(event.type == "click"){
-                        switchScript("startMenu", "level1", "level 1.js");
+document.onkeydown = function(e) {
+    switch (e.keyCode) {
+        case 38: // Up arrow
+            redCircle.y -= 10;
+            break;
+        case 40: // Down arrow
+            redCircle.y += 10;
+            break;
+        case 37: // Left arrow
+            redCircle.x -= 10;
+            break;
+        case 39: // Right arrow
+            redCircle.x += 10;
+            break;
+        case 32: // Space bar
+            for (let i = 0; i < circles.length; i++) {
+                if (Math.abs(redCircle.x - circles[i].x) <= 100) {
+                    if (circles[i].dx === 0) {
+                        circles[i].dx = 2;
+                    } else {
+                        circles[i].dx = 0;
                     }
-                } else if (levelInfo.on) {
-                    // Reset box color
-                    ctx.fillStyle = levelInfo.color;
-                    ctx.fillRect(x - (boxSize / 2), y - (boxSize / 2), boxSize, boxSize);
-                    
-                    ctx.fillStyle = "black";
-                    ctx.font = "30px Arial";
-                    ctx.textAlign = "center";
-                    ctx.textBaseline = "middle";
-                    ctx.fillText(levelInfo.text, x, y);
                 }
             }
-        }
+            break;
+    }
 };
-
